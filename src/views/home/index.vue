8e9d9296b36cd6a777b432a6b1459496d9e2c566
xuhuan <ruirui1994@icloud.com>
2020-01-08 10:13:38 +0800

    绘制品字主页面

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>后台首页展示</div>
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -7,4 +13,16 @@ export default {}
 </script>
 
 <style lang="less" scoped>
+.el-container {
+  height: 100%;
+  .el-header {
+    background-color: #B3C0D1;
+  }
+  .el-aside {
+    background-color: #D3DCE6;
+  }
+  .el-main {
+    background-color: #E9EEF3;
+  }
+}
 </style>
